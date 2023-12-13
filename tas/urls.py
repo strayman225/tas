609b4fa9772035d47ea09520d61e5c0b6131d4ed
jijiluzana <133578376+jijiluzana@users.noreply.github.com>
2023-12-13 10:29:58 +0800

    sd

--- tas/urls.py
+++ tas/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.views.static import serve
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
